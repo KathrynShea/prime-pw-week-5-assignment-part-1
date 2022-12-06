408d072a71e90e9506e170156144c22de395f388
Kathryn Szombatfalvy <kathrynszombatfalvy@gmail.com>
2022-12-05 20:23:12 -0600

    final

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -15,7 +15,7 @@ const me = {
   hasSiblings: true,
   shoeCount: 7,
   favThreeFoods: ['pasta', 'sushi', 'chocolate'],
-};
+}
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
@@ -25,14 +25,14 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 let fullName = me.firstName + " " + me.lastName;
-console.log(fullName);
+console.log('My name is', fullName);
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-console.log(me.favThreeFoods[0]);
-console.log(me.favThreeFoods[2]);
+console.log('My first favorite food is', me.favThreeFoods[0] + '.');
+console.log('My last favorite food is', me.favThreeFoods[2] + '.');
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -42,11 +42,11 @@ console.log(me.favThreeFoods[2]);
 */
 console.log('I have', me.shoeCount, 'shoes.');
 me.shoeCount++;
-console.log('I have', me.shoeCount, 'shoes.');
+console.log('Now I have', me.shoeCount, 'shoes!');
 
 
 /* 5. Add a new property to an existing object.
-  - Add a 'favoriteColor' to your existing object
+  - Add a 'favoriteColxsor' to your existing object
   - Do this the same way you updated the variable above
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
